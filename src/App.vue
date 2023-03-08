30299d81cdd1f189af96b034fe84a1faf47460c8
csleong98 <macintoshleong@gmail.com>
2023-03-09 06:57:06 +0800

    remove the default elements

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,10 @@ import Button from './components/Button.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
   <main>
     <Cards />
     <Button />
-    <TheWelcome />
   </main>
 </template>
 
