311634f94923d92aa1d39dcefd3c0757c5d44b49
csleong98 <macintoshleong@gmail.com>
2023-03-08 17:27:58 +0800

    styled the card component

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -5,9 +5,11 @@
 <template>
     <div class="card">
         <div class="card-header">
-            <h2>I'm a Card!</h2>
+            <h2>Title</h2>
         </div>
         <div class="card-body">
+            <div class="card-image">
+            </div>
             <span>This is the body of the card</span>
         </div>
         <div class="card-footer">
@@ -18,15 +20,26 @@
 
 <style>
 .card {
+    background-color: aliceblue;
+    border-radius: 8px;
     height: 200px;
     width: 150px;
+    padding: 16px 16px;
 }
 .card-header > h2 {
-    color: blueviolet;
+    color: rgb(42, 42, 42);
+    font-size: x-large;
+    font-weight: 600;
+}
+
+.card-body > .card-image {
+    background-image: url("https://fujifilm-x.com/wp-content/uploads/2021/01/gfx100s_sample_04_thum-1.jpg");
+    background-color: azure;
+    background-size: 100%;
 }
 
 .card-body > span{
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     font-size: medium;
+    color: rgb(42, 42, 42);
 }
 </style>
