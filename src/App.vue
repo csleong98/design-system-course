b965629409982fdff7a4f21c4aaf0b494aa85e4c
csleong98 <macintoshleong@gmail.com>
2023-03-08 11:31:47 +0800

    created card component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Cards from './components/Cards.vue';
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
+    <Cards />
     <TheWelcome />
   </main>
 </template>
---
+++ src/components/Cards.vue
@@ -0,0 +1,32 @@
+<script>
+ export default {}
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-header">
+            <h2>I'm a Card!</h2>
+        </div>
+        <div class="card-body">
+            <span>This is the body of the card</span>
+        </div>
+        <div class="card-footer">
+            <span>Button component should be here</span>
+        </div>
+    </div>
+</template>
+
+<style>
+.card {
+    height: 200px;
+    width: 150px;
+}
+.card-header > h2 {
+    color: blueviolet;
+}
+
+.card-body > span{
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: medium;
+}
+</style>
